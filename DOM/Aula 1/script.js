6a2bc08bab49082ab72cddf3afe03d90fbabd9a9
Joao-Carlos2002 <jcsra2002@gmail.com>
2022-10-16 12:15:26 -0300

    Exercicios

---
+++ DOM/Aula 1/script.js
@@ -0,0 +1,12 @@
+function show() {
+    let contatos = document.getElementById('contact-list')
+    console.log(contatos)
+
+    let elementosDeLista = document.getElementsByTagName('li')
+    console.log(elementosDeLista)
+
+    let inputsDeContato = document.getElementsByClassName('contact-input')
+    console.log(inputsDeContato)
+
+    let formulario = document.forms()
+}
